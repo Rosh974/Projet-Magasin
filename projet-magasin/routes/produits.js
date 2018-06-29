7b5615742684151f4a2b25396760f643eacaf7ee
anna974 <annapiaraly@gmail.com>
2018-06-29 08:52:50 +0400

    ajout des routes

---
+++ projet-magasin/routes/produits.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+// A MODIFIER SI BESOIN !!!
+
+var produit = require("../app.js");
+
+//recuperer les produits
+router.get("/", produit.list);
+
+//voir un produit par son id
+router.get("/show/:id", produit.show);
+
+//cree un produit
+router.get("/create", produit.create);
+
+//sauvegarder un produit. /!\ cest un POST 
+router.post("/save", produit.save);
+
+//editer un produit
+router.get("/edit/:id", produit.edit);
+
+//edit update.  /!\ cest un POST 
+router.post("/update/:id", produit.update);
+
+
+//export du module router
+module.exports = router;
+
