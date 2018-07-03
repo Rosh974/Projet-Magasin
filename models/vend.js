dfdffee7914735d30eca13e41f58d28ea3374944
tnk976 <badrane976@gmail.com>
2018-07-03 12:09:55 +0400

    creation magasin controllers et model vend

--- models/vend.js
+++ models/vend.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var vendSchema = new mongoose.Schema({
-    id_produit:[{ type: Schema.Types.ObjectId, ref: 'Produit' }]
+    id_produit:[{ type: Schema.Types.ObjectId, ref: 'Produit' }],
     id_magasin: [{ type: Schema.Types.ObjectId, ref: 'Magasin' }]
 });
 
